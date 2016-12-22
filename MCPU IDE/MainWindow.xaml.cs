e58d1892e841b662dac9ab08a89debee6786de18
Unknown6656 <unknown84@gmx.de>
2016-12-22 18:15:46 +0100

    Inital IDE commit

---
+++ MCPU IDE/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Media.Imaging;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Data;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System;
+
+namespace MCPU.IDE
+{
+    using MCPU.Compiler;
+    using MCPU;
+
+
+    public partial class MainWindow
+        : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
