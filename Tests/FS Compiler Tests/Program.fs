10a56d9bb3bc3a18c16ef056536160759741bfdd
Unknown6656 <unknown84@gmx.de>
2016-12-28 00:24:33 +0100

    Added first testing project

---
+++ Tests/FS Compiler Tests/Program.fs
@@ -0,0 +1,12 @@
+﻿namespace testing
+
+open MCPU
+open MCPU.Compiler
+
+module FSTests =
+    [<EntryPoint>]
+    let main argv =
+        let code = @""
+        let res = MCPUCompiler.Compile code
+        
+        0
