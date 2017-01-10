9a29a7a053c46b9f553e8577987190cc411bb7c6
Unknown6656 <unknown84@gmx.de>
2017-01-10 23:37:35 +0100

    Started to implement the AST string builder

--- MCPU++/MCPU++ Parser/SyntaxTree.fs
+++ MCPU++/MCPU++ Parser/SyntaxTree.fs
@@ -39,16 +39,16 @@ and UnaryOperator =
     | Identity
 and Arguments = Expression list
 and Expression =
+    | LiteralExpression of Literal
     | ScalarAssignmentExpression of IdentifierRef * Expression
     | PointerAssignmentExpression of IdentifierRef * Expression
     | ArrayAssignmentExpression of IdentifierRef * Expression * Expression
     | BinaryExpression of Expression * BinaryOperator * Expression
     | UnaryExpression of UnaryOperator * Expression
     | IdentifierExpression of IdentifierRef
-    | ArrayIdentifierExpression of IdentifierRef * Expression
     | FunctionCallExpression of Identifier * Arguments
+    | ArrayIdentifierExpression of IdentifierRef * Expression
     | ArraySizeExpression of IdentifierRef
-    | LiteralExpression of Literal
     | ArrayAllocationExpression of VariableType * Expression
     | PointerAllocationExpression of IdentifierRef
     | PointerValueIdentifierExpression of IdentifierRef
@@ -72,3 +72,35 @@ and Declaration =
     | GlobalVarDecl of VariableDeclaration
     | FunctionDeclaration of FunctionDeclaration
 and Program = Declaration list
+
+
+
+module Builder =
+    let rec Build (ast : obj) =
+        let inline (</) f x = (Build >> f) x
+        match box ast with
+        | :? Program as p -> p
+                             |> List.map (fun x -> Build x)
+                             |> String.concat "\n"
+        | :? Literal as l -> match l with
+                             | IntLiteral i -> i.ToString()
+                             | FloatLiteral f -> f.ToString()
+        | :? Identifier as i -> i
+        | :? IdentifierRef as r -> r.Identifier
+        | :? VariableType as t -> match t with
+                                  | Float -> "float"
+                                  | Int -> "int"
+                                  | Unit -> "void"
+        | :? VariableDeclaration as v ->
+            let t, s, i = match v with
+                          | ArrayDeclaration(t, i) -> (t, "[]", i)
+                          | PointerDeclaration(t, i) -> (t, "*", i)
+                          | ScalarDeclaration(t, i) -> (t, "", i)
+            sprintf "%s%s %s" </ t <| s </ i
+        | :? Parameters as p -> p
+                                |> List.map (fun x -> Build x)
+                                |> String.concat ", "
+        | :? FunctionDeclaration as t ->
+            let r, i, p, b = t
+            sprintf "function %s %s(%s)\n{\n%s\n}" </ r </ i </ p </ b
+        | _ -> failwith "The type cannot be matched"
